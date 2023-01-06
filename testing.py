51310bfe201cb41628a96e66f475de1660117578
Rakai Erlans Fatahillah <rakaief@gmail.com>
2023-01-06 19:11:00 +0700

    Other testing

--- testing.py
+++ testing.py
@@ -1,3 +1,6 @@
+def testing():
+    return "Hello worl"
+
 x = input('input:')
 x = int(x)
 if x>2:
@@ -5,3 +8,19 @@ if x>2:
 elif x<2:
     print("Kurang dari 2")
 else: print(x)
+
+try:
+    while x<-1:
+        x = -2
+        print(printasd2)
+    print("Looping")
+except: print("NaN")
+
+for i in range(1,10,1):
+    print(i)
+
+print(0 is 0.0)
+print(testing())
+
+
+
